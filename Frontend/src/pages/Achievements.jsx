a39acce340734f0e5c4602ad56bf48f4e88de10c
Adarsh Roy <adarshroy2010@gmail.com>
2025-02-09 04:31:56 +0530

    Dark mode added in all pages

--- Frontend/src/pages/Achievements.jsx
+++ Frontend/src/pages/Achievements.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useTheme } from '../ThemeContext';
+
 
 function Achievements() {
-  return (
-    <div>Achievements</div>
-  )
+  const { isDark } = useTheme();
+    return (
+      <div className={`min-h-[88vh] ${isDark ? 'bg-[#1a1b1e] text-white' : 'bg-white text-gray-900'} transition-colors duration-200 `}>
+  
+  
+  
+  
+        <div className='flex justify-end mt-8 mr-2 md:mt-0 md:absolute md:bottom-2 md:right-24'>
+          <p className={`${isDark ? 'text-gray-300' : 'text-gray-500'} text-xs md:text-sm`}>© {new Date().getFullYear()} Adarsh Roy. All Rights Reserved</p>
+        </div>
+      </div>
+    )
 }
 
 export default Achievements
