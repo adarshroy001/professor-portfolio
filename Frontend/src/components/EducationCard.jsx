71e6f82d4b3ff526c0d02a3ffdc4890631467838
Adarsh Roy <adarshroy2010@gmail.com>
2025-02-09 05:51:59 +0530

    Education Route done

---
+++ Frontend/src/components/EducationCard.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { useTheme } from '../ThemeContext';
+
+
+function EducationCard({ eduDetail }) {
+    console.log(eduDetail);
+    const { isDark } = useTheme();
+
+
+    return (
+
+        <div className="w-full overflow-hidden m-auto pt-4 sm:pt-8 pb-4 sm:pb-8 ">
+            <div className="Title w-[85%] border-b sm:w-[90%] lg:w-[80vw] mx-auto relative">
+                <p className={`${isDark ? 'text-gray-400' : 'text-gray-600'} text-lg sm:text-2xl font-semibold mx-3`}>
+                  { eduDetail.DegreeName}
+                </p>
+            </div>
+            <div className="Bottom-half w-[90%] lg:w-[80vw] mx-auto mt-4 grid grid-cols-1 lg:grid-cols-2 sm:grid-cols-2">
+                <div className="image w-full mt-0 sm:mt-4">
+                    <div className=' rounded-xl !ml-4 sm:!ml-8 w-[85%] sm:w-[90%] xl:w-[75%] !h-[100%]'>
+                        <img src={eduDetail.collegeImg} alt="" className='rounded-xl object-fill w-full h-full p-1' />
+                    </div>
+                </div>
+
+                <div className="text-part ml-5 w-full">
+                    <p className={`${isDark ? 'text-gray-400' : 'text-gray-600'} text-base mt-3 mb-0 sm:mb-3 w-4/5`}>
+                    { eduDetail.CollegeName}
+                    </p>
+                    <p className={`${isDark ? 'text-gray-400' : 'text-gray-600'} text-base mt-3  mb-0 sm:mb-3 w-4/5`}>
+                    { eduDetail.Department}
+                    </p>
+                    <p className={`${isDark ? 'text-gray-400' : 'text-gray-600'} text-base mt-3  mb-0 sm:mb-3 w-4/5`}>
+                    { eduDetail.Title}
+                    </p>
+                    <p className={`${isDark ? 'text-gray-400' : 'text-gray-600'} text-base mt-3  mb-0 sm:mb-3 w-4/5`}>
+                        <span className='text-base font-bold'>Supervisor:  </span>   { eduDetail.Supervisor}
+                    </p>
+                    <p className={`${isDark ? 'text-gray-400' : 'text-gray-600'} text-base mt-3  mb-0 sm:mb-3 w-4/5`}>
+                        <span className='text-base font-bold'>Year:  </span>   { eduDetail.year}
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default EducationCard
--- Frontend/src/pages/Educations.jsx
+++ Frontend/src/pages/Educations.jsx
@@ -1,18 +1,56 @@
 import React from 'react'
 import { useTheme } from '../ThemeContext';
+import EducationCard from '../components/EducationCard';
 
 
 function Educations() {
   const { isDark } = useTheme();
+  const educationDetail = [
+    {
+      id: '1',
+      'DegreeName': 'Doctor of Philosophy (PhD)',
+      'CollegeName': ' Indian Institute of Technology (IIT) Bombay, Maharashtra, India',
+      'Department': 'Metallurgical Engineering and Materials Science',
+      'Title': 'Analysis of deformation behavior of glassy polymers during nanoindentation',
+      'Supervisor': 'Prof. Prita Pant',
+      'year': '2015-2024',
+      'collegeImg': 'https://www.iitb.ac.in/sites/www.iitb.ac.in/files/styles/slick_media/public/2023-12/P1120458_0.jpg?itok=OpEalU1f'
+    },
+    {
+      id: '2',
+      'DegreeName': 'Master of Engineering (ME)',
+      'CollegeName': 'Indian Institute of Science (IISc) Bangalore, Karnataka, India',
+      'Department': 'Materials Engineering',
+      'Title': 'Preparation and mechanical properties of nanoparticles embedded polymer films',
+      'Supervisor': 'Prof. Ashok M. Raichur',
+      'year': '2010-2012',
+      'collegeImg': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs56GWUUnvsQtGrT60mOajwpebdJ_J3AAw9w&s'
+
+    },
+    {
+      id: '3',
+      'DegreeName': 'Bachelor of Engineering (BE)',
+      'CollegeName': ' Jadavpur University (JU) Kolkata, West Bengal, India',
+      'Department': 'Metallurgical and Material Engineering',
+      'Title': 'Development of aluminum alloys',
+      'Supervisor': 'Prof. Akshay K. Pramanick',
+      'year': '2006-2010',
+      'collegeImg': 'https://jadavpuruniversity.in/storage/2021/03/JadavpurVarsity-gigapixel-standard-scale-2_00x.jpg'
+
+    },
+    
+  ]
   return (
     <div className={`min-h-[88vh] ${isDark ? 'bg-[#1a1b1e] text-white' : 'bg-white text-gray-900'} transition-colors duration-200 `}>
+      {
+        educationDetail.map((item,index)=>(
+          <EducationCard key ={item.id} eduDetail ={ item}/>
+        )
 
+        )
+      }
+      
 
-
-
-      <div className='flex justify-end mt-8 mr-2 md:mt-0 md:absolute md:bottom-2 md:right-24'>
-        <p className={`${isDark ? 'text-gray-300' : 'text-gray-500'} text-xs md:text-sm`}>© {new Date().getFullYear()} Adarsh Roy. All Rights Reserved</p>
-      </div>
     </div>
   )
 }
