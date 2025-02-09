09a659b00e89e3a8d7de8fc0835a086326e378c9
Adarsh Roy <adarshroy2010@gmail.com>
2025-02-09 13:51:48 +0530

    Admin panel Folder along with tailwind

---
+++ AdminPanel/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <div className='bg-pink-500 h-screen'>
+        Hello tailwind
+       </div>
+    </>
+  )
+}
+
+export default App
